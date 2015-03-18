c216f66a86de2751b0642399142d4de49a13fd8e
divramod <divramod@gmail.com>
2015-03-18 14:56:15 +0100

    added sudo to zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,7 @@
-apt-get install zsh
-apt-get install git-core
+sudo apt-get install zsh
+sudo apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-chsh -s `which zsh`
+sudo chsh -s `which zsh`
 cd /usr/local/share/zsh
 sudo chmod -R 755 ./site-functions
 sudo chown -R root:root ./site-functions
