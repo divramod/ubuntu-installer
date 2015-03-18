9f01a1702943079ff8947bb146dfe62af54f57bf
divramod <divramod@gmail.com>
2015-03-18 14:45:23 +0100

    update

--- zsh.sh
+++ zsh.sh
@@ -2,3 +2,6 @@ apt-get install zsh
 apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
+cd /usr/local/share/zsh
+sudo chmod -R 755 ./site-functions
+sudo chown -R root:root ./site-functions
