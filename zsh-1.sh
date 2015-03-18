3f1b90689914615e9f01820a4622025b29f0c1bd
divramod <divramod@gmail.com>
2015-03-18 14:58:50 +0100

    divided zsh in post and pre

--- zsh-1.sh
+++ zsh-1.sh
@@ -1,7 +1,3 @@
 sudo apt-get install zsh
 sudo apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-sudo chsh -s `which zsh`
-cd /usr/local/share/zsh
-sudo chmod -R 755 ./site-functions
-sudo chown -R root:root ./site-functions
---
+++ zsh-2.sh
@@ -0,0 +1,4 @@
+sudo chsh -s `which zsh`
+cd /usr/local/share/zsh
+sudo chmod -R 755 ./site-functions
+sudo chown -R root:root ./site-functions
